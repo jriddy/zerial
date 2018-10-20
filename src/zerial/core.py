b7f515272ad642b96da39da8e8eb155e35629223
Josh Reed <jriddy@gmail.com>
2018-10-20 13:43:43 -0400

    reimpl the first steps of the destructure recurse

--- src/zerial/core.py
+++ src/zerial/core.py
@@ -1,7 +1,16 @@
 import attr
 
 
-deztructure = attr.asdict
+def deztructure(inst, _dict_factory=dict):
+    fields = attr.fields(inst.__class__)
+    ret = _dict_factory()
+    for field in fields:
+        value = getattr(inst, field.name)
+        if attr.has(value.__class__):
+            ret[field.name] = deztructure(value, _dict_factory)
+        else:
+            ret[field.name] = value
+    return ret
 
 
 def reztructure(typ, dct):
--- tests/integration/test_ztructure.py
+++ tests/integration/test_ztructure.py
@@ -1,4 +1,5 @@
 import attr
+import pytest
 
 from zerial import deztructure, reztructure
 
@@ -20,3 +21,28 @@ def test_deztructure_basic():
 
 def test_reztrucutre_basic():
     assert reztructure(Point3D, p3d_dct) == p3d_obj
+
+
+@attr.s
+class LineSegment3D(object):
+    p1 = attr.ib(type=Point3D)
+    p2 = attr.ib(type=Point3D)
+
+
+linseg_obj = LineSegment3D(
+    Point3D(0., 1., 2.),
+    Point3D(1., 2., 0.),
+)
+lineseg_dct = {
+    'p1': {'x': 0., 'y': 1., 'z': 2.},
+    'p2': {'x': 1., 'y': 2., 'z': 0.},
+}
+
+
+def test_deztructure_recursive():
+    assert deztructure(linseg_obj) == lineseg_dct
+
+
+@pytest.mark.skip
+def test_rezstructure_recursive():
+    assert reztructure(LineSegment3D, lineseg_dct) == linseg_obj
