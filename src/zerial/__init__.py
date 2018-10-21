8fc895baa40df9d751388d08a8c6fbeb0c3eddf5
Josh Reed <jriddy@gmail.com>
2018-10-21 12:05:22 -0400

    move core functions to object for coding convenience

--- src/zerial/__init__.py
+++ src/zerial/__init__.py
@@ -1,9 +1,14 @@
 from ._data import zdata, Zequence
-from ._core import deztructure, reztructure
+from ._core import Ztructurer
 
 __author__ = 'Josh Reed'
 __email__ = 'jriddy@gmail.com'
 __version__ = '0.0.5'
+
+
+_the_ztructurer = Ztructurer()
+deztructure = _the_ztructurer.deztructure
+reztructure = _the_ztructurer.reztructure
 
 
 __all__ = [
--- src/zerial/_core.py
+++ src/zerial/_core.py
@@ -1,26 +1,30 @@
 import attr
 
 
-def deztructure(inst, _dict_factory=dict):
-    fields = attr.fields(inst.__class__)
-    ret = _dict_factory()
-    for field in fields:
-        value = getattr(inst, field.name)
-        if attr.has(field.type):
-            ret[field.name] = deztructure(value, _dict_factory)
-        else:
-            ret[field.name] = value
-    return ret
+@attr.s
+class Ztructurer(object):
+    dict_factory = attr.ib(default=dict)
+
+    def deztructure(self, inst):
+        fields = attr.fields(inst.__class__)
+        ret = self.dict_factory()
+        for field in fields:
+            value = getattr(inst, field.name)
+            if attr.has(field.type):
+                ret[field.name] = self.deztructure(value)
+            else:
+                ret[field.name] = value
+        return ret
 
 
-def reztructure(klass, mapping):
-    # TODO: implement efficiency compat for py2 without six if possible
-    fields = attr.fields(klass)
-    kwargs = {}
-    for field in fields:
-        data = mapping[field.name]
-        if attr.has(field.type):
-            kwargs[field.name] = reztructure(field.type, data)
-        else:
-            kwargs[field.name] = data
-    return klass(**kwargs)
+    def reztructure(self, klass, mapping):
+        # TODO: implement efficiency compat for py2 without six if possible
+        fields = attr.fields(klass)
+        kwargs = {}
+        for field in fields:
+            data = mapping[field.name]
+            if attr.has(field.type):
+                kwargs[field.name] = self.reztructure(field.type, data)
+            else:
+                kwargs[field.name] = data
+        return klass(**kwargs)
