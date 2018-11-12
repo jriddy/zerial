ffbfd833efbc0297e464573855b6437198224915
Josh Reed <jriddy@gmail.com>
2018-11-12 13:50:04 -0500

    add mapping ztype destructurer

---
+++ tests/test_zapping.py
@@ -0,0 +1,33 @@
+from typing import Any
+
+import attr
+import pytest
+
+from zerial._data import Zapping
+
+
+def test_anykey_is_wrapper_around_key_type():
+    assert Zapping.anykey(int) == Zapping(Any, int)
+
+
+def test_destruct_simple_dict(ztr):
+    zap = Zapping.anykey(int, str)
+    dct = {1: 'a', 2: 'b', 3: 'c'}
+    dez = zap.destruct(dct, ztr)
+    assert dez == dct
+
+
+@attr.s
+class Counter(object):
+    state = attr.ib(type=int)
+
+
+def test_destruct_zapping_recursive_simple(ztr):
+    zap = Zapping(str, Counter)
+    val = {k: Counter(ord(k)) for k in 'ABC'}
+    dez = zap.destruct(val, ztr)
+    assert dez == {
+        'A': {'state': 65},
+        'B': {'state': 66},
+        'C': {'state': 67},
+    }
