870c5a2ea56dacbbc22914f9cdd6334ffc205575
Josh Reed <jriddy@gmail.com>
2018-11-15 11:30:31 -0500

    improve zendthru failure message - addresses #9

--- src/zerial/_data.py
+++ src/zerial/_data.py
@@ -205,7 +205,9 @@ class Zendthru(_Ztype):
     def _check_type(self, inst):
         if not isinstance(inst, self.pass_type):
             raise TypeError(
-                '{!r} is not an instance of {!r}'.format(inst, self.pass_type)
+                '{!r} is an instance of {!r}, not {!r}'.format(
+                    inst, type(inst), self.pass_type,
+                )
             )
 
     def destruct(self, inst, _ztr):
