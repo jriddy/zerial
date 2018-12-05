3ab75df228dfcb93f06c3da656889a1cc71e185f
Josh Reed <jriddy@gmail.com>
2018-12-05 12:32:05 -0500

    Bump version: 0.1.5 → 0.1.6

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='zerial',
-    version='0.1.5',
+    version='0.1.6',
     description='Stuct and destructure complex classes',
     long_description=readme + '\n\n' + doclink + '\n\n' + history,
     author='Josh Reed',
--- src/zerial/__init__.py
+++ src/zerial/__init__.py
@@ -3,7 +3,7 @@ from ._core import Ztructurer
 
 __author__ = 'Josh Reed'
 __email__ = 'jriddy@gmail.com'
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 
 _the_ztructurer = Ztructurer()
